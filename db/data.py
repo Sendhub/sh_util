0f58125ab1229d1c5f608e346b45fd496f12908a
Brock Haywood <brock@sendhub.com>
2013-11-26 14:44:13 -0800

    Don't mess with the shard moving stuff yet

--- db/data.py
+++ db/data.py
@@ -239,7 +239,7 @@ def tableRowCounts(tableColumnPairs, userIdOrUserIds, using):
     # True if userIdOrUserIds is an iterable, otherwise False.
     isIterable = isinstance(userIdOrUserIds, (set, list))
 
-    sql = ' UNION ALL '.join(map(
+    sql = ' UNION '.join(map(
         lambda (table, column): toSingleLine(
             '''
                 SELECT '{table}' "table", COUNT(*) "count"
