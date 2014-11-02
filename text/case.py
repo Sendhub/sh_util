25e389b9a64e2373322662c12f9cc63749c484f4
Brock Haywood <brock.haywood@gmail.com>
2014-11-02 04:53:14 +0000

    formatting

--- text/case.py
+++ text/case.py
@@ -20,7 +20,18 @@ Relevant StackOverflow question: http://stackoverflow.com/a/1176023/293064
 ...         }
 ...     }
 ... })
-{'a_camel_case_key': 'aCamelValue', 'c': {'d_nested_camel_case_key': 'dNeCa', 'e': 'e', 'f': {'g_ultra_nested_camel_c_ase': 'GInnerNestedCamelCAse', 'h': 'h'}}, 'b': 'b'}
+{
+    'a_camel_case_key': 'aCamelValue',
+    'c': {
+        'd_nested_camel_case_key': 'dNeCa',
+        'e': 'e',
+        'f': {
+            'g_ultra_nested_camel_c_ase': 'GInnerNestedCamelCAse',
+            'h': 'h'
+        }
+    },
+    'b': 'b'
+}
 """
 
 __author__ = 'Jay Taylor [@jtaylor]'
@@ -32,6 +43,7 @@ import re
 _underscorer1 = re.compile(r'(.)([A-Z][a-z]+)')
 _underscorer2 = re.compile('([a-z0-9])([A-Z])')
 
+
 def camelToSnake(s):
     """
     Is it ironic that this function is written in camel case, yet it
@@ -42,6 +54,7 @@ def camelToSnake(s):
 
 
 _snakeFinder = re.compile(r'_(\w)')
+
 
 def snakeToCamel(s):
     """Yet this is not ironic.."""
@@ -64,7 +77,18 @@ def dictKeysToSnakeCase(struct):
     ...         }
     ...     }
     ... })
-    {'a_camel_case_key': 'aCamelValue', 'c': {'d_nested_camel_case_key': 'dNeCa', 'e': 'e', 'f': {'g_ultra_nested_camel_c_ase': 'GInnerNestedCamelCAse', 'h': 'h'}}, 'b': 'b'}
+    {
+        'a_camel_case_key': 'aCamelValue',
+        'c': {
+            'd_nested_camel_case_key': 'dNeCa',
+            'e': 'e',
+            'f': {
+                'g_ultra_nested_camel_c_ase': 'GInnerNestedCamelCAse',
+                'h': 'h'
+                }
+            },
+        'b': 'b'
+    }
     """
     t = type(struct)
 
@@ -77,7 +101,7 @@ def dictKeysToSnakeCase(struct):
         # into a dict, then do so
         if t is not dict:
             struct = struct.to_dict()
-            
+
         for k, v in struct.items():
             del struct[k]
             struct[camelToSnake(k)] = dictKeysToSnakeCase(v)
@@ -86,10 +110,14 @@ def dictKeysToSnakeCase(struct):
     elif t is list or hasattr(struct, '__iter__'):
         return [dictKeysToSnakeCase(item) for item in struct]
 
+    elif struct is None:
+        return None
+
     else:
         raise Exception(
             '_dictKeysToSnakeCase: unsupported type `{0}\''.format(t)
         )
+
 
 def dictKeysToCamelCase(struct):
     """
@@ -132,4 +160,3 @@ if __name__ == '__main__':
     assert camelToSnake('snakes_on_a_plane') == 'snakes_on_a_plane'
     assert camelToSnake('IPhoneHysteria') == 'i_phone_hysteria'
     assert camelToSnake('iPhoneHysteria') == 'i_phone_hysteria'
-
