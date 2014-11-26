d3b32a96a6e06e995aca59c050d1099b1e37e6fc
Brock Haywood <brock.haywood@gmail.com>
2014-11-26 18:03:53 +0000

    fixed a bug where isInteger didnt work on negative numbers

--- types_util.py
+++ types_util.py
@@ -4,21 +4,29 @@ IntTypes = (types.IntType, types.LongType)
 NumberTypes = (types.IntType, types.LongType,
                types.FloatType, types.ComplexType)
 
-def isNumber(n):
-    ret = (n is not None and
-           (isinstance(n, NumberTypes) or
-            (isinstance(n, basestring) and n.isdigit())))
+def isNumber(maybe_num):
+    ret = (maybe_num is not None and
+           (isinstance(maybe_num, NumberTypes) or
+            (isinstance(maybe_num, basestring) and maybe_num.isdigit())))
 
     if ret is False:
         try:
-            float(n)
+            float(maybe_num)
             ret = True
         except (ValueError, TypeError):
             pass
     return ret
 
-def isInteger(n):
-    return n is not None and \
-           (isinstance(n, IntTypes) or
-            (isinstance(n, basestring) and
-             n.isdigit()))
+def isInteger(maybe_num):
+
+    ret = False
+
+    if maybe_num:
+        ret = isinstance(maybe_num, IntTypes)
+
+        if not ret and isinstance(maybe_num, basestring):
+            maybe_num = maybe_num.strip()
+            ret = maybe_num.isdigit() or \
+                  (maybe_num[0] in "+-" and maybe_num[1:].isdigit())
+
+    return ret
